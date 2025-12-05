cf60073101356dc48acfb2128cc96c7b745079f7
Florent PELLEGRIN <dev@folpe.me>
2025-12-05 17:28:26 +0100

    feat: add Umami analytics script to RootLayout for tracking user interactions

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,18 @@
 import "styles/tailwind.css"
+
+import Script from "next/script"
 import { ClientLayout } from "./ClientLayout"
 
 export default async function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html>
       <body>
+        <Script
+          defer
+          src="https://umami.volpionas.synology.me/u.js"
+          data-website-id="ff4e3096-3c26-42a6-85c4-707ac1f8a28f"
+          strategy="afterInteractive"
+        />
         <ClientLayout>{children}</ClientLayout>
       </body>
     </html>
